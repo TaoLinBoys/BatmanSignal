e7e0b4e3f5d91f90ab027e9ba5d4a2087361cff6
Leon Loi <leonloi3278@gmail.com>
2016-11-09 15:26:27 -0500

    WIP

---
+++ Signal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+
+
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    printf("program exited due to SIGINT\n");
+    exit(0);
+  }
+  if(signo == SIGUSR1) printf("Parent PID: %d, " , getppid());
+    
+}
+
+void main(){
+  
+  signal(SIGINT,sighandler);
+  signal(SIGUSR1, sighandler);
+  while(1){
+    kill(getpid(), SIGUSR1);
+    printf("PID: %d\n", getpid());
+    sleep(1);
+    
+  }
+}
