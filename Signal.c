0a3e4d2185a6c37df38ad063301e527a25e62978
Tao Lin <lin.tao.5711y@gmail.com>
2016-11-09 20:46:09 -0500

    now writes to a file

--- Signal.c
+++ Signal.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <signal.h>
+#include <fcntl.h>
 
 
 static void sighandler(int signo){
   if(signo == SIGINT){
-    printf("program exited due to SIGINT\n");
+    umask(0);
+    int fd = open("closeinfo", O_CREAT | O_WRONLY | O_APPEND, 0644);
+    write(fd, "program exited due to SIGINT", 29);
+    close(fd);
+    
     exit(0);
   }
   if(signo == SIGUSR1) printf("Parent PID: %d, " , getppid());
